7809538e33559585a04d01f2cc634bc115c74071
Skarginson <102185297+Skarginson@users.noreply.github.com>
2024-05-01 18:22:49 +0200

    delete button wip
    Co-authored-by: GuillaumeDurame <GuillaumeDurame@users.noreply.github.com>

--- Apartment Rentals Admin/src/App.jsx
+++ Apartment Rentals Admin/src/App.jsx
@@ -1,5 +1,6 @@
 import logo from "./assets/main-logo.png";
 import "./App.css";
+import RentalList from "./components/RentalList";
 
 function App() {
   return (
@@ -8,49 +9,7 @@ function App() {
       <main>
         <Sidebar />
         <div className="main-div">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dictum
-          turpis in eleifend maximus. Integer vehicula lectus sed lectus
-          bibendum convallis. Ut tempus est at tempor posuere. Donec lacus enim,
-          varius at augue vitae, tempor molestie arcu. Vivamus vestibulum arcu
-          id ornare consectetur. Donec ut mi eget est dictum convallis.
-          Vestibulum tincidunt augue arcu, at malesuada nibh pretium non.
-          Vivamus quis sem nec mauris congue luctus at id lorem. Nam vel nisi a
-          eros convallis pretium. Maecenas tincidunt condimentum ante. Donec
-          varius velit vel ex pretium ornare. Nulla et mattis erat, sit amet
-          facilisis nulla. Pellentesque pulvinar felis eget mollis interdum. Sed
-          sed purus mi. Duis laoreet pulvinar urna mattis finibus. Aliquam
-          bibendum libero quis eros rutrum posuere. Maecenas tincidunt purus
-          suscipit, pellentesque purus sit amet, placerat orci. Donec ut turpis
-          aliquam, blandit tellus sit amet, volutpat ligula. Nam et condimentum
-          nunc. Nunc aliquet massa diam, at lacinia ante vehicula ut. Cras vitae
-          libero metus. Cras porta nunc nec mauris imperdiet, in venenatis dolor
-          accumsan. Interdum et malesuada fames ac ante ipsum primis in
-          faucibus. Nam diam lacus, ullamcorper vitae faucibus in, venenatis a
-          nisi. Nam et tortor libero. Aliquam pulvinar porta sapien eu lobortis.
-          Cras a dolor dapibus, finibus odio non, auctor erat. Donec hendrerit
-          dignissim odio. Vivamus eleifend erat non lacus sagittis tristique.
-          Nulla eu nunc lacinia, malesuada augue non, luctus erat. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Ut at sollicitudin enim.
-          Etiam finibus viverra cursus. Nullam urna tortor, placerat non
-          tincidunt eget, elementum ac ex. Cras accumsan tortor a tincidunt
-          porta. Mauris molestie, elit vitae posuere lacinia, magna odio rhoncus
-          ipsum, eget fringilla erat massa at neque. Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Duis elementum tortor nec ligula aliquam,
-          vitae elementum libero ornare. Nulla vel euismod ante, non condimentum
-          nunc. In nibh ipsum, tincidunt in justo quis, pellentesque posuere
-          velit. Vestibulum cursus, magna vitae rutrum tempor, tortor ex
-          fermentum tellus, a rutrum erat felis eget est. In fringilla sapien
-          nunc, sit amet placerat quam interdum eu. In hac habitasse platea
-          dictumst. Sed aliquam ex quis nisl imperdiet commodo. Phasellus
-          molestie tincidunt neque, eu dapibus libero tristique ac. Duis
-          venenatis dignissim convallis. Donec sagittis ullamcorper ex a
-          maximus. In hac habitasse platea dictumst. Ut vel efficitur ante.
-          Nulla vitae justo risus. Fusce in elementum sem, vitae congue orci.
-          Morbi ac porta lectus. Phasellus semper lobortis ligula ac ornare.
-          Donec eget bibendum purus, ac aliquam nunc. Cras quis malesuada risus,
-          et vehicula arcu. Suspendisse ultricies, quam nec condimentum
-          vulputate, arcu elit sodales purus, et consectetur libero sapien ut
-          erat.
+          <RentalList />
         </div>
       </main>
       <Footer />
@@ -94,4 +53,5 @@ function Sidebar() {
     </div>
   );
 }
+
 export default App;
---
+++ Apartment Rentals Admin/src/components/RentalList.jsx
@@ -0,0 +1,45 @@
+import rentals from "../assets/rentals.json";
+import villachere from "../assets/villachere.jpg";
+import FlagFrance from "../assets/Flag_of_France.png";
+import FlagSpain from "../assets/Flag_of_Spain.png";
+import { useState } from "react";
+
+function RentalList() {
+  const [rentalsAvailable, setRentals] = useState(rentals);
+
+  function deleteRentals(name) {
+    const rentalsToKeep = rentalsAvailable.results.filter(
+      (rentals) => rentals.name !== name
+    );
+    setRentals(rentalsToKeep);
+  }
+  return (
+    <div className="rentals-list">
+      <h2>Rentals Lists</h2>
+      {rentals.results.map((el) => {
+        return (
+          <div key={el.id}>
+            <p className="Name"> {el.name}</p>
+            <p className="Localisation">
+              {el.country} - {el.city}
+            </p>
+            <p className="Details">
+              {el.bathrooms}
+              {el.bedrooms}
+              {el.price}
+              {el.property_type}
+            </p>
+            <button onClick={() => deleteRentals(el.name)}>Delete</button>
+            <img src={villachere} />
+            {el.country === "France" && <img src={FlagFrance} />}
+            {el.country === "Spain" && <img src={FlagSpain} />}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default RentalList;
+
+// property type : afficher ou pas un petit logo.
